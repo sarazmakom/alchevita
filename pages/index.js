81db45969ed533b930ca1b7763df97d3e34c28f2
Sara Zmak <zmaksara@gmail.com>
2025-05-02 16:56:02 +0200

    create layout and titlebar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-export default function HomePage() {
+import useSWR from "swr";
+
+export default function Home() {
+  const { data, isLoading, error } = useSWR("/api/remedies", {
+    fallbackData: [],
+  });
   return (
     <div>
       <h1>Hello from Next.js</h1>
