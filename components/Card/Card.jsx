86852e2b7fa53ff394c1bf80e679fc760fd7daa1
sham dowaji <shamrdowaji@gmail.com>
2025-05-07 15:05:06 +0200

    removed the changes from Card and SymptomList components

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -25,7 +25,6 @@ export default function Card({ title, imageUrl, id, symptoms }) {
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             alt={`Visual representation of ${title}`}
-            priority
           />
         </ImageContainer>
       </Link>
