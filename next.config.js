0aa1581ec3fd288d9f603c12a14e3f0b39745c31
Girish Vadlamudi <girishvadlamudi@outlook.com>
2025-05-14 11:42:54 +0200

    Image upload to create remedy

--- next.config.js
+++ next.config.js
@@ -9,10 +9,20 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dhckhirkm/**",
       },
     ],
   },
