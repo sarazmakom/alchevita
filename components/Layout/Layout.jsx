74c53e000e71679ef58fc67ace27346516bf1d45
Sara Zmak <zmaksara@gmail.com>
2025-05-20 19:26:53 +0200

    background color main

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -5,6 +5,7 @@ import Navbar from "../Navbar/Navbar";
 const MainContent = styled.main`
   padding-bottom: 5rem;
   padding-top: 4rem;
+  background-color: var(--background);
 `;
 
 export default function Layout({ title = "", children }) {
