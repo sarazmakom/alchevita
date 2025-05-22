89b9b5d9b6a00694a40643aa48943de485e6ede2
Sara Zmak <zmaksara@gmail.com>
2025-05-22 12:36:31 +0200

    add more color vars

--- styles.js
+++ styles.js
@@ -7,6 +7,11 @@ export default createGlobalStyle`
   --secondary: #62D2A2;
   --background: #D7FBE8;
   --text-dark: #0E6F5D;
+  --color-danger-bg: #f8d7da;
+  --color-danger-text: #e20a40;
+  --color-danger-border: #eb979f;
+  --color-danger-hover-bg: #f1c0c4;
+  --color-disabled: #92a29f;
   }
 
   *,
